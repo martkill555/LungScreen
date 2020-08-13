0ce2721b40aba88afeaa9c8e4a5feceddf686770
Sarawut Rattanawichein <36756986+martkill555@users.noreply.github.com>
2020-08-13 09:31:16 +0700

    x

--- lungscan/components/Drawer.vue
+++ lungscan/components/Drawer.vue
@@ -1,37 +1,63 @@
 <template>
   <v-card
     id="inspire" class="myFont"
-   
-    color="grey lighten-4"
+   color="#AD2323"
+    dark
     flat
-    height="200px"
+    height="800"
     tile
+    width="500"
   >
+   <v-card-title dark>
+<v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+    <img src="../store/logo.png" width="30" height="30"> <h3  id="inspire" class="myFont" >LUNGSCAN</h3>
+    
+    <v-spacer> </v-spacer>
+     <nuxt-link to="/ProfileView"> <v-btn rounded color="#D60000" id="inspire" class="myFont" >View Profile</v-btn></nuxt-link>  
+      
+    </v-card-title>
+ <v-divider class="my-3"></v-divider>
+ <div  align="center"
+      justify="center">
+<v-container></v-container><v-container></v-container>
+
+ <v-container>
+   <v-img
+      class="img-center"
+      height="150px"
+      width="150px"
+      src="https://scontent.fbkk22-1.fna.fbcdn.net/v/t1.0-9/p960x960/70286121_2507184436030743_4153033461684240384_o.jpg?_nc_cat=100&_nc_sid=85a577&_nc_eui2=AeFmHi3XXYNO64vAJFmkTg9eR9E0bJitpYlH0TRsmK2lidu8qt4yX6MjOsxZ_mefcktgaMuk5jmr6nLEGhJ4n8N8&_nc_ohc=SyOnbf2Rz6sAX84H2fN&_nc_ht=scontent.fbkk22-1.fna&_nc_tp=6&oh=f1f566c07a7fd282b0e71f62f7be01fe&oe=5F5B8D9D"
+    >
+    
+       </v-img> 
+       <v-container></v-container>
+       
+       <v-container>
+         
+       <nuxt-link to="/Screen"> <v-btn  x-large color="#3E6896" id="inspire" class="myFont" >Lung Screen <v-icon  dark right>mdi-cloud-upload</v-icon></v-btn></nuxt-link>  
+       
+       
+        </v-container>  
+ </v-container>
+ <nuxt-link to="/History"> <v-btn x-large color="#3E6896" id="inspire" class="myFont" >History</v-btn></nuxt-link>
+ </div>
   
     <v-app-bar
-      color="#A32E2E"
+      color="#AD2323"
+      height="55"
       dark
     >
-      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+      
+      
 
-       <img src="../store/logo.png" width="30" height="30">
        
-
-       <nuxt-link to="/Profile">  <v-btn text x-large color="primary" dark>Lungscan</v-btn></nuxt-link>
-
-      <v-spacer></v-spacer>
-
-   
-  
-   
-    <nuxt-link to="/ProfileView"> <v-btn text large color="primary" dark >Profile</v-btn> </nuxt-link> 
-    <nuxt-link to="/">  <v-btn text large color="primary" dark>Logout</v-btn></nuxt-link>
     </v-app-bar>
 
     <v-navigation-drawer
       v-model="drawer"
       absolute
       temporary
+      height="500"
     >
       <v-list
         nav
--- lungscan/pages/Profile.vue
+++ lungscan/pages/Profile.vue
@@ -5,54 +5,16 @@
 
  
 <Footer/>
-<Drawer/>
-<v-container>
-   <v-row>
-<v-card
-    class="mx-auto"
-    color="#AD2323"
-    dark
-    width="500"
-    height="500"
-  >
-    <v-card-title dark>
- 
-    <img src="../store/logo.png" width="30" height="30"> <h3  id="inspire" class="myFont" dark>LUNGSCAN</h3>
-    
-    <v-spacer> </v-spacer>
-     <nuxt-link to="/ProfileView"> <v-btn rounded color="#D60000" id="inspire" class="myFont" >View Profile</v-btn></nuxt-link>  
-      
-    </v-card-title>
- <v-divider class="my-3"></v-divider>
- <div  align="center"
-      justify="center">
-<v-container></v-container><v-container></v-container>
+<Nav/>
 
- <v-container>
-   <v-img
-      class="img-center"
-      height="150px"
-      width="150px"
-      src="https://scontent.fbkk22-1.fna.fbcdn.net/v/t1.0-9/p960x960/70286121_2507184436030743_4153033461684240384_o.jpg?_nc_cat=100&_nc_sid=85a577&_nc_eui2=AeFmHi3XXYNO64vAJFmkTg9eR9E0bJitpYlH0TRsmK2lidu8qt4yX6MjOsxZ_mefcktgaMuk5jmr6nLEGhJ4n8N8&_nc_ohc=SyOnbf2Rz6sAX84H2fN&_nc_ht=scontent.fbkk22-1.fna&_nc_tp=6&oh=f1f566c07a7fd282b0e71f62f7be01fe&oe=5F5B8D9D"
-    >
-    
-       </v-img> 
-       <v-container></v-container>
-       
-       <v-container>
-         
-       <nuxt-link to="/Screen"> <v-btn  x-large color="#3E6896" id="inspire" class="myFont" >Lung Screen <v-icon  dark right>mdi-cloud-upload</v-icon></v-btn></nuxt-link>  
-       
-       
-        </v-container>  
- </v-container>
- <nuxt-link to="/History"> <v-btn x-large color="#3E6896" id="inspire" class="myFont" >History</v-btn></nuxt-link>
- </div>
-</v-card>
+
+   <v-row>
+      <Drawer/>
+
 
  <v-card
     
-    width="1280"
+    width="1420"
     height="500"
     
   >
@@ -122,7 +84,7 @@
   </v-card>
 
   </v-row>
-  </v-container>
+  
 
 
 </div>
